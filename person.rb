a24d969b9439d9a02befef87487ca6b77587ff96
Bria222 <techconsultants.bree@gmail.com>
2022-09-30 11:53:41 +0300

    added output file, student,teacher,and trimmer decorator

---
+++ person.rb
@@ -0,0 +1,28 @@
+require('./nameable')
+
+class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
