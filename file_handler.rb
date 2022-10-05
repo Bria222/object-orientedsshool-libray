8d4165976b84c755d5067d3dedac00dfd6b9e8a3
Mesi21 <mse212002@gmail.com>
2022-10-05 15:43:41 +0300

    :sparkles: add read_people method

--- file_handler.rb
+++ file_handler.rb
@@ -8,3 +8,23 @@ def read_book(file)
     add_book(book_title, book_author)
   end
 end
+def read_people(file)
+  puts "\n**********************PEOPLE************************\n"
+  file.each do |el|
+    case el['value']['type']
+    when 'Student'
+      student_classroom = el['value']['classroom']
+      student_age = el['value']['age']
+      student_name = el['value']['name']
+      has_permission = el['value']['has_permission']
+      puts "Classroom: #{student_classroom} Student Age: #{student_age} Student Name #{student_name}"
+      add_student(student_classroom, student_age, student_name, has_permission)
+    when 'Teacher'
+      teacher_specialization = el['value']['specialization']
+      teacher_age = el['value']['age']
+      teacher_name = el['value']['name']
+      puts "Teacher Specialization: #{teacher_specialization} Age: #{teacher_age} Teacher's Name #{teacher_name}"
+      add_teacher(teacher_specialization, teacher_age, teacher_name, true)
+    end
+  end
+end
